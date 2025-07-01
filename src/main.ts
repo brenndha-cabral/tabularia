bd2dc65579abf43ce9152e6bc60a4c1ed442ed4f
brenndha-cabral <cabralbrenndha@gmail.com>
2025-07-01 10:22:34 -0300

    build: config envs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+  const port = config.get<number>('PORT', 3000);
+
+  process.env.TZ = config.get<string>('TIMEZONE');
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  await app.listen(port);
 }
+
 bootstrap();
